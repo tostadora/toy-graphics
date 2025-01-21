0e9bfc62610c5039a81269b21fe1b414126ab2bc
Luis C. Gallego <tostadora@gmail.com>
2025-01-21 09:51:25 +0000

    change the box positions and added the z-proyection

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ struct Point (i32, i32, i32);
 
 fn main() {
     let corners: [Point; 8] = [
-        Point( 0, 0,  0),
-        Point(12, 0,  0),
-        Point(12, 8,  0),
-        Point( 0, 8,  0),
-        Point( 0, 0, 10),
-        Point(12, 0, 10),
-        Point(12, 8, 10),
-        Point( 0, 8, 10),
+        Point( 1, -1, -5),
+        Point( 1, -1, -3),
+        Point( 1,  1, -5),
+        Point( 1,  1, -3),
+        Point(-1, -1, -5),
+        Point(-1, -1, -3),
+        Point(-1,  1, -5),
+        Point(-1,  1, -3),
     ];
 
+    for point in corners {
+        println!("Proyected corner: x: {}, y: {})", point.0 as f64 / -point.2 as f64, point.1 as f64 / -point.2 as f64);
+    }
 }
